7017dffe10961f8ccd49c0662ca5706a3b5999a1
Nadia Ramadhani <103446529+nadiaramadhani@users.noreply.github.com>
2022-08-28 22:40:21 +0700

    Initial Commit

---
+++ FindMeow/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FindMeow
+//
+//  Created by Local Administrator on 28/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FindMeow/FindMeowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindMeowApp.swift
+//  FindMeow
+//
+//  Created by Local Administrator on 28/8/22.
+//
+
+import SwiftUI
+
+@main
+struct FindMeowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
